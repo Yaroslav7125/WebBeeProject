7789083285ad793df7a4c8d8080684f9f7655016
Yaroslav <bahanovaroslav@gmail.com>
2021-07-20 18:43:50 +0300

    Add pages for normal routing

--- index.js
+++ index.js
@@ -1,97 +1,88 @@
 let url1 = "https://yaroslav7125.github.io/SPA.io/ajax/first.html";
 let url2 = "http://localhost:3000/templates/main-screen.html";
 let url3 = "http://localhost:3000/templates/main-screen-map.html";
-let elem = document.getElementById('content');
-
-
-console.log(elem);
 
 
 
-let xhttp = new XMLHttpRequest(); 
 
 
-xhttp.onreadystatechange = function(){
-    if(this.readyState==4&&this.status == 200){  // если ответ от сервера есть и он хороший - 20
-        myFunction(this.responseText); // тогда вызываем функцию myFunction
-    }
-}
+
+
+
+
+
 
 /* console.log("make xhttp");
 xhttp.open("Get", "https://yaroslav7125.github.io/SPA.io/ajax/first.html");
 xhttp.send(); */
 
-let number = 1;
-
-let myFunction = data => {
-    console.log(data+ `${number++}`);                   /*отладка*/ 
-    let elm = document.getElementById("content");
-    console.log(elm);
-    elm.innerHTML = data;
 
 
 
+
+function getPath(str){
+    return str.split('/')[str.split('/').length-1];
+}
+
+///----------------------------------------------------------------------
+divElem = document.querySelector(".nav"); // navbar в котором находятся ссылки
+content = document.getElementById('content') // div элемент куда вставляется контент
+let xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function(){
+    console.log(`Status is ${this.status}`);
+    if(this.readyState==4&&this.status == 200){  // если ответ от сервера есть и он хороший - 20
+        ShowContent(this.responseText); // тогда вызываем функцию myFunction
+    }
+}
+const ShowContent = data => {
+    console.log(data);
+    content.innerHTML = data;
 }
 let makeGetRequest = uri =>{
     xhttp.open("Get", uri);
     xhttp.send();
 }
 
-
-
-let callFucnk = ()=>{
-    makeGetRequest(`${url2}`);
-} 
-
-
-
 //-------------------------------------------------------------------------
 
-
-divElem = document.querySelector(".nav");
-console.log(divElem);       /*отладка*/
-content = document.getElementById('content')
-
-
-
-function Router(ThePath){
+let flag = false;
+function GetContent(ThePath){
     switch(ThePath){
 
-        /* case '/':
-            history.pushState(null, null, ThePath)
-            content.innerHTML = "TheMain";
-            break; */
-        case '/map':
+         case '/':
+            makeGetRequest(`${location.origin}/templates${ThePath}activity.html`);
+            break; 
+        case '/map.html':
             //history.pushState(null, null, ThePath)
-            content.innerHTML = 'TheMap';
+            makeGetRequest(`${location.origin}/templates${ThePath}`);
             break;
-        case '/activity':
+
+        case '/activity.html':
             //history.pushState(null, null, ThePath)
-            content.innerHTML = "TheActivity";
+            makeGetRequest(`${location.origin}/templates${ThePath}`);
             break;
-        case '/time':
-            
-            content.innerHTML = "theTime";
+        case '/time.html':
+            makeGetRequest(`${location.origin}/templates${ThePath}`);
             break;    
     };  
-    console.log(ThePath);
-}
-//Router(location.pathname);
+    
 
+    //console.log(ThePath);                     //отлавдка
+}
+
+console.log(location.pathname)
+GetContent(location.pathname);
 divElem.addEventListener('click', function(e){
-    
-    
     if(e.target.tagName!=="A")return;
-    
     let path = e.target.getAttribute("href");
+    
     history.pushState(null, null, path);
-    Router(path);  
+    GetContent(path);  
     e.preventDefault();  
 });
 window.addEventListener('popstate',function(){
-    console.log(location.pathname);
-
-    Router(location.pathname); 
+    //console.log(location.pathname);
+    GetContent(location.pathname); 
     
 });
 

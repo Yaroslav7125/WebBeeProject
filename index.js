50b7927281a5839a484afd5c959f2eec21f8c7a8
Yaroslav7125 <bahanovaroslav@gmail.com>
2021-07-21 21:10:15 +0300

    rewrote css for a in header and rewrote js

--- index.js
+++ index.js
@@ -1,36 +1,14 @@
-let url1 = "https://yaroslav7125.github.io/SPA.io/ajax/first.html";
-let url2 = "http://localhost:3000/templates/main-screen.html";
-let url3 = "http://localhost:3000/templates/main-screen-map.html";
-
-
-
-
-
-
-
-
-
-
-
-/* console.log("make xhttp");
-xhttp.open("Get", "https://yaroslav7125.github.io/SPA.io/ajax/first.html");
-xhttp.send(); */
-
-
-
-
-
 function getPath(str){
     return str.split('/')[str.split('/').length-1];
 }
-
-///----------------------------------------------------------------------
 divElem = document.querySelector(".nav"); // navbar в котором находятся ссылки
 content = document.getElementById('content') // div элемент куда вставляется контент
 
 const ShowContent = data => {
     content.innerHTML = data;
-    showTime();
+    if(document.getElementById('timer') != null){
+        document.getElementById('timer').innerHTML =  `${hours}:${minutes}:${sec}`;
+    }
 }
 let makeGetRequest = (uri, cb) =>{
     let xhttp = new XMLHttpRequest();
@@ -39,7 +17,6 @@ let makeGetRequest = (uri, cb) =>{
 
     
     xhttp.onreadystatechange = function(ev){
-        console.log(`Status is ${this.status}`);
         if(this.readyState==4&&this.status == 200){  // если ответ от сервера есть и он хороший - 20
             cb && cb(this.responseText);
             // ShowContent(this.responseText); // тогда вызываем функцию myFunction
@@ -53,16 +30,12 @@ let makeGetRequest = (uri, cb) =>{
 let flag = false;
 function GetContent(ThePath){
     switch(ThePath){
-
          case '/':
             makeGetRequest(`${location.origin}/templates${ThePath}activity.html`, (data) => ShowContent(data));
             break; 
         case '/map.html':
-            //history.pushState(null, null, ThePath)
             makeGetRequest(`${location.origin}/templates${ThePath}`, (data) => {
                 ShowContent(data); // тогда вызываем функцию myFunction
-                
-                
                 DG.then(function() {
                     map = DG.map('map', {
                         'center': [54.98, 82.89],
@@ -71,7 +44,6 @@ function GetContent(ThePath){
                     navigator.geolocation.getCurrentPosition((pos)=>{
                         coord = pos.coords;
                         DG.marker([coord.latitude, coord.longitude]).addTo(map);
-                        // map.flyTo([coord.latitude,coord.longitude],13);
                         map.setView([coord.latitude,coord.longitude],17);
                     });
                     
@@ -89,11 +61,7 @@ function GetContent(ThePath){
             break;    
     };  
     
-
-    //console.log(ThePath);                     //отлавдка
 }
-
-console.log(location.pathname)
 GetContent(location.pathname);
 divElem.addEventListener('click', function(e){
     if(e.target.tagName!=="A")return;
@@ -104,50 +72,33 @@ divElem.addEventListener('click', function(e){
     e.preventDefault();  
 });
 window.addEventListener('popstate',function(){
-    //console.log(location.pathname);
     GetContent(location.pathname); 
     
 });
 ///////////////////////////////////timer
-let int_timer = [0,0,0];
-
 let sec = 0;
 let hours = 0;
 let minutes = 0;
-
 document.addEventListener("DOMContentLoaded", ()=>{
     if(document.getElementById('timer') != null){
         document.getElementById('timer').innerHTML =  `${hours}:${minutes}:${sec}`;
     }
 });
-
 showTime = function(){
-    if (document.getElementById('timer') == null)return;
     if(sec==60){sec=0; minutes +=1;}
     if(minutes==60){minute=0;hours+=1};
     sec+=1;
-    if(document.getElementById('timer') != null){
-        document.getElementById('timer').innerHTML =  `${hours}:${minutes}:${sec}`;
-    }
-    
+    if (document.getElementById('timer') == null)return;
+    document.getElementById('timer').innerHTML =  `${hours}:${minutes}:${sec}`;   
 }
-
-
-
-
 function Timer(){
     let timerId = setInterval(()=> showTime(), 1000);
 }
-
-
-
-
 document.addEventListener("DOMContentLoaded", Timer);
+showTime(); // при переходе должен быть запущен таймер
 
 
 
 
 
-
-
-
+console

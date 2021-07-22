8f0636bb11373b1be63715670c00eead175c8353
Yaroslav <bahanovaroslav@gmail.com>
2021-07-22 17:32:52 +0300

    Add backlight for menu.

--- index.js
+++ index.js
@@ -3,30 +3,20 @@ function getPath(str){
 }
 divElem = document.querySelector(".nav"); // navbar в котором находятся ссылки
 content = document.getElementById('content') // div элемент куда вставляется контент
-
 const ShowContent = data => {
     content.innerHTML = data;
-    if(document.getElementById('timer') != null){
-        document.getElementById('timer').innerHTML =  `${hours}:${minutes}:${sec}`;
-    }
+    showTime();
 }
 let makeGetRequest = (uri, cb) =>{
     let xhttp = new XMLHttpRequest();
     xhttp.open("Get", uri);
     xhttp.send();
-
-    
     xhttp.onreadystatechange = function(ev){
         if(this.readyState==4&&this.status == 200){  // если ответ от сервера есть и он хороший - 20
             cb && cb(this.responseText);
-            // ShowContent(this.responseText); // тогда вызываем функцию myFunction
         }
     }
-
 }
-
-//-------------------------------------------------------------------------
-
 let flag = false;
 function GetContent(ThePath){
     switch(ThePath){
@@ -49,9 +39,7 @@ function GetContent(ThePath){
                     
                 });
             });
-            
             break;
-
         case '/activity.html':
             //history.pushState(null, null, ThePath)
             makeGetRequest(`${location.origin}/templates${ThePath}`, (data) => ShowContent(data));
@@ -63,42 +51,39 @@ function GetContent(ThePath){
     
 }
 GetContent(location.pathname);
-divElem.addEventListener('click', function(e){
-    if(e.target.tagName!=="A")return;
-    let path = e.target.getAttribute("href");
-    
-    history.pushState(null, null, path);
-    GetContent(path);  
-    e.preventDefault();  
+
+let links = document.querySelectorAll(".actions-href");
+links.forEach((elm)=> {
+    elm.addEventListener('click', function(e){
+        e.preventDefault(); 
+        let path = elm.getAttribute("href");
+        links.forEach((link)=>{
+            link.classList.remove('active');
+        });
+        elm.classList.add('active');
+        history.pushState(null, null, path);
+        GetContent(path);  
+    })
 });
 window.addEventListener('popstate',function(){
     GetContent(location.pathname); 
-    
 });
-///////////////////////////////////timer
+//timer
 let sec = 0;
-let hours = 0;
-let minutes = 0;
-document.addEventListener("DOMContentLoaded", ()=>{
-    if(document.getElementById('timer') != null){
-        document.getElementById('timer').innerHTML =  `${hours}:${minutes}:${sec}`;
-    }
-});
 showTime = function(){
-    if(sec==60){sec=0; minutes +=1;}
-    if(minutes==60){minute=0;hours+=1};
-    sec+=1;
-    if (document.getElementById('timer') == null)return;
-    document.getElementById('timer').innerHTML =  `${hours}:${minutes}:${sec}`;   
+    let timer = document.getElementById('timer');
+    if (timer == null)return;
+    let secStr = String(sec%60).padStart(2, '0');
+    let hoursStr = String(Math.trunc(sec/3600)).padStart(2, '0');
+    let minutesStr = String(Math.trunc(sec/60)).padStart(2, '0');
+    timer.innerHTML =  `${hoursStr}:${minutesStr}:${secStr}`;   
 }
 function Timer(){
-    let timerId = setInterval(()=> showTime(), 1000);
+    let timerId = setInterval(()=> /* showTime() */countTime(), 1000);
+}
+function countTime(){
+    sec++;
+    showTime();
 }
 document.addEventListener("DOMContentLoaded", Timer);
 showTime(); // при переходе должен быть запущен таймер
-
-
-
-
-
-console
